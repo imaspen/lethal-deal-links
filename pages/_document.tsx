55f9341352a3737dc9e4537786687998f396d8e2
Aspen <zebington@gmail.com>
2022-06-02 14:47:33 +0100

    Twitter graphs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,11 +22,28 @@ export default function Document() {
         <link rel="icon" href="/path/to/favicon-192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/favicon-180.png" sizes="180x180" />
 
-        <meta property="og:title" content="Lethal Deal Links" />
+        <meta property="og:title" content="Lethal Deal" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://lethaldeal.imaspen.dev/" />
         <meta
           property="og:image"
+          content="https://lethaldeal.imaspen.dev/splash.png"
+        />
+        <meta
+          name="og:description"
+          content="Links to the portfolios of Lethal Deal's development team."
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="lethaldeal.imaspen.dev" />
+        <meta property="twitter:url" content="https://lethaldeal.imaspen.dev" />
+        <meta name="twitter:title" content="Lethal Deal" />
+        <meta
+          name="twitter:description"
+          content="Links to the portfolios of Lethal Deal's development team."
+        />
+        <meta
+          name="twitter:image"
           content="https://lethaldeal.imaspen.dev/splash.png"
         />
       </Head>
