532b8547da265ba73687280c96dbbc144c2e7d3d
Aspen <zebington@gmail.com>
2022-06-07 09:13:06 +0100

    fix favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,9 +17,9 @@ export default function Document() {
 
         <meta name="theme-color" content="#7c1e21" />
 
-        <link rel="icon" href="/path/to/favicon-32.png" sizes="32x32" />
-        <link rel="icon" href="/path/to/favicon-128.png" sizes="128x128" />
-        <link rel="icon" href="/path/to/favicon-192.png" sizes="192x192" />
+        <link rel="icon" href="/favicon-32.png" sizes="32x32" />
+        <link rel="icon" href="/favicon-128.png" sizes="128x128" />
+        <link rel="icon" href="/favicon-192.png" sizes="192x192" />
         <link rel="apple-touch-icon" href="/favicon-180.png" sizes="180x180" />
 
         <meta property="og:title" content="Lethal Deal" />
